275ace1ea84ad824e00743d93f0b979b4aadd65e
Rina Dz <dz.rina.na@gmail.com>
2023-06-08 01:25:48 +0300

    попытка 4

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,7 @@ const AuthError = require('../utils/errors/auth-err');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new AuthError('Необходима авторизация'));
+    return next(new AuthError('Необходима авторизация (нет поля authorization)'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new AuthError('Необходима авторизация'));
+    return next(new AuthError('Необходима авторизация (почему-то токен не проходит варификацию)'));
   }
 
   req.user = payload;
